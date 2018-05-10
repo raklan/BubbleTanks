34dcff7f79d19ef024a41404a60b86b5a2ac7819
raklan <rcl.catcher@gmail.com>
2018-05-10 13:40:48 -0600

    New Tank

--- FinalProject/src/Tank.java
+++ FinalProject/src/Tank.java
@@ -1,13 +1,28 @@
+import javax.swing.*;
+
 public class Tank extends Sprite{
 
+    private JLabel tur;
+
     public Tank(){
-        super("src/resources/Lvl1.png");
+        super("src/resources/Lvl1_Vert.png");
+        tur = new JLabel(new ImageIcon("src/resources/Lvl1_Vert.png"));
+        add(tur);
+        tur.setVisible(true);
+        tur.setBounds(0,0,this.getWidth(), this.getHeight());
+    }
+
+    public Tank(int x, int y){
+        super("src/resources/Lvl1_Vert.png");
+        tur = new JLabel(new ImageIcon("src/resources/Lvl1_Vert.png"));
+        add(tur);
+        tur.setVisible(true);
+        tur.setBounds(0,0,this.getWidth(), this.getHeight());
+        setLocation(x,y);
     }
 
     @Override
     public void move() {
-        x+=dx;
-        y+=dy;
-        setLocation(x,y);
+        setLocation(x+dx,y+dy);
     }
 }
