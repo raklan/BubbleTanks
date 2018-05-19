ca09085abb5326c1554f0e986fdb5441fc23d573
raklan <rcl.catcher@gmail.com>
2018-05-18 23:06:06 -0600

    Revert "Revert "New Collision""
    This reverts commit d1323c1d27c7e2784ff14d82a6bd23493bd02cc9.

---
+++ FinalProject/src/Shop.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Shop extends JFrame {
+
+    private JLabel title;
+
+    private JButton speedUpgrade;
+    private JButton bulletSpeed;
+    private JButton maxHP;
+    private JButton life;
+
+    public Shop(){
+
+    }
+}
